665f29215fb7382273cae9eb9b977d8dd8f87008
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-02-14 19:49:28 +0100

    jobeet partly removed

--- apps/frontend/modules/job/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>Edit Jobeet job</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -2,42 +2,13 @@
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <title>Jobeet - Your best job board</title>
+    <title>Twitarch - Export and save your tweets</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_javascripts() ?>
     <?php include_stylesheets() ?>
   </head>
   <body>
-    <div id="container">
-      <div id="header">
-        <div class="content">
-          <h1><a href="<?php echo url_for('job/index') ?>">
-            <img src="/images/logo.jpg" alt="Jobeet Job Board" />
-          </a></h1>
- 
-          <div id="sub_header">
-            <div class="post">
-              <h2>Ask for people</h2>
-              <div>
-                <a href="<?php echo url_for('job/index') ?>">Post a Job</a>
-              </div>
-            </div>
- 
-            <div class="search">
-              <h2>Ask for a job</h2>
-              <form action="" method="get">
-                <input type="text" name="keywords"
-                  id="search_keywords" />
-                <input type="submit" value="search" />
-                <div class="help">
-                  Enter some keywords (city, country, position, ...)
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
- 
+    <div id="container"> 
       <div id="content">
         <?php if ($sf_user->hasFlash('notice')): ?>
           <div class="flash_notice">
@@ -59,16 +30,15 @@
       <div id="footer">
         <div class="content">
           <span class="symfony">
-            <img src="/images/jobeet-mini.png" />
-            powered by <a href="http://www.symfony-project.org/">
+            Twitarch is powered by <a href="http://www.symfony-project.org/">
             <img src="/images/symfony.gif" alt="symfony framework" />
             </a>
           </span>
           <ul>
-            <li><a href="">About Jobeet</a></li>
+            <li><a href="">About Twitarch</a></li>
             <li class="feed"><a href="">Full feed</a></li>
-            <li><a href="">Jobeet API</a></li>
-            <li class="last"><a href="">Affiliates</a></li>
+            <li><a href="">Twitarch API</a></li>
+            <!-- <li class="last"><a href="">Affiliates</a></li> -->
           </ul>
         </div>
       </div>
