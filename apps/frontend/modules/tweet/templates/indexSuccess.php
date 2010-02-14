651648a5f217c78afda01b49e7a065d4f0babe2c
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-02-14 20:25:06 +0100

    route fixed

--- apps/frontend/modules/tweet/templates/indexSuccess.php
+++ apps/frontend/modules/tweet/templates/indexSuccess.php
@@ -1,25 +1,16 @@
-<h2>Tweets List</h2>
-
 <table>
   <thead>
     <tr>
-      <th>Id</th>
-      <th>User</th>
-      <th>Text</th>
-      <th>Created at</th>
-      <th>Updated at</th>
+      <th>Tweets</th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($tweets as $tweet): ?>
     <tr>
-      <td><a href="<?php echo url_for('tweet_show', $tweet) ?>"><?php echo $tweet->getId() ?></a></td>
-      
-      <td><?php echo $tweet->getUserId() ?></td>
-      <td><?php echo $tweet->getText() ?></td>
+      <td><a href="<?php echo url_for('tweet_show', $tweet) ?>"><?php echo $tweet->getText() ?></a></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
 
-  <a href="<?php echo url_for('tweet_new') ?>">New</a>
+  <a href="<?php echo url_for('@tweet_new') ?>">New</a>
