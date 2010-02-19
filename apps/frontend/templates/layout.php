fdd559d23351239ee4c1a9351f1753e9d2b7ee12
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-02-19 15:03:21 +0100

    menu edited

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -2,7 +2,7 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <head>
-    <title><?php include_slot('title', 'Twitarch - save and export your tweets') ?></title>
+    <title><?php include_slot('title', 'TweetEx - Export your Tweets') ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_javascripts() ?>
     <?php include_stylesheets() ?>
@@ -10,36 +10,50 @@
   <body>
     <div id="wrapper">
       <div id="header">
-        <p id="skip"><a href="#body">Skip Navigation</a></p>
-        <div id="rss">
-        	 <dl>
-			 <dt>RSS Feeds:</dt>
-               <dd><a href="#" title="Syndicate your tweets using RSS">Tweets</a></dd>
-               <dd><a href="#" title="The latest added users in RSS">Users</a></dd>
-             </dl>
-		</div>
-        <div id="search">
-        	<form action="<?php echo url_for('user/searchTweets')?>" method="get">
-                    <p>
-                        <label for="twitterUsername">Twitter Username:</label>
-                        <input type="text" name="twitterUsername" id="s" size="15"/>
-                        <button type="submit">Export your Tweets</button>
-                    </p>
-                </form>
-		</div>
-		<h1 id="headline"><a href="<?php echo url_for('@homepage') ?>">Twitarch</a></h1>
-        <div id="navigation">
-        <ul>
-          <li><a href="<?php echo url_for('@homepage') ?>">Home</a></li>
-          <li><a href="<?php echo url_for('tweet/index') ?>">Tweets</a></li>
-          <li><a href="<?php echo url_for('user/index') ?>">Users</a></li>
-          <li><a href="<?php echo url_for('geolocation/index') ?>">Geo Locations</a></li>
-          <li><a href="<?php echo url_for('source/index') ?>">Tweet Sources</a></li>
-          <!-- <li><a href="<?php echo url_for('user/searchTweets') ?>">Search Tweets</a></li> -->
-        </ul>
-        </div>
-      </div>
+		<h1 id="headline"><a href="<?php echo url_for('@homepage') ?>">TweetEx - Export your Tweets</a></h1>
+            <ul id="nav"> 
+				<li class="current"><a href="<?php echo url_for('@homepage') ?>">Home</a></li> 
+				<li><a href="<?php echo url_for('tweet/index') ?>">Tweets</a></li> 
+			    <li><a href="<?php echo url_for('user/index') ?>">User</a></li>
+				<li><a href="<?php echo url_for('source/index') ?>">Sources</a></li>
+				<li><a href="<?php echo url_for('geolocation/index') ?>">Geo Locations</a></li> 
+			
+				<!--  
+				<li><a href="#">Multi-Levels</a> 
+					<ul> 
+						<li><a href="#">Team</a> 
+							<ul> 
+								<li><a href="#">Sub-Level Item</a></li> 
+								<li><a href="#">Sub-Level Item</a> 
+									<ul> 
+										<li><a href="#">Sub-Level Item</a></li> 
+										<li><a href="#">Sub-Level Item</a></li> 
+										<li><a href="#">Sub-Level Item</a></li> 
+									</ul> 
+								</li> 
+								
+								<li><a href="#">Sub-Level Item</a></li> 
+							</ul> 
+						</li> 
+						<li><a href="#">Sales</a></li> 
+						<li><a href="#">Another Link</a></li> 
+						<li><a href="#">Department</a> 
+							<ul> 
+								<li><a href="#">Sub-Level Item</a></li> 
+								<li><a href="#">Sub-Level Item</a></li> 
+								<li><a href="#">Sub-Level Item</a></li> 
+							</ul> 
+						</li> 
+					</ul> 
+				</li>	
+				 
+				<li><a href="<?php echo url_for('@homepage') ?>">About</a></li> 
+				<li><a href="<?php echo url_for('@homepage') ?>">Contact Us</a></li>
+				--> 
+		  </ul>	 
+	</div>
     <div id="body">
+    
       <div id="content">
       <?php if ($sf_user->hasFlash('notice')): ?>
           <div class="flash_notice">
@@ -55,6 +69,7 @@
         
         <?php echo $sf_content ?>
       </div>
+      <p>Menu by <a href="http://www.webdesignerwall.com/">WDW</a>, <a href="http://www.webdesignerwall.com/demo/css3-dropdown-menu/">E</a></p>
     </div>
     </div>
   </body>
