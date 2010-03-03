842d4dc718aba9ebb559d70281b673428b0da632
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-03-03 11:15:59 +0100

    pagination updated

--- apps/frontend/modules/tweet/templates/indexSuccess.php
+++ apps/frontend/modules/tweet/templates/indexSuccess.php
@@ -43,6 +43,7 @@
       <td><?php echo $tweet->getCreatedAt() ?></td>
       <td><?php echo $tweet->getUpdatedAt() ?></td>
     </tr>
+    <?php $twitterUser = $tweet->getTweetUser()?>
     <?php endforeach; ?>
   </tbody>
 </table>
@@ -54,23 +55,23 @@
   <?php if($pager->getPage() == 1): ?>
   	<li class="previous-off">&lt; Previous</li>
   <?php else: ?>
- 	 <li class="previous"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getFirstPage() ?>">&laquo; First page</a></li>
- 	 <li class="previous"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getPreviousPage() ?>"><img src="/images/previous.png" title="Previous page" alt="Previous page"/></a></li>
+ 	 <li class="previous"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getFirstPage() ?>&n=<?php echo $twitterUser ?>">&laquo; First page</a></li>
+ 	 <li class="previous"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getPreviousPage() ?>&n=<?php echo $twitterUser ?>"><img src="/images/previous.png" title="Previous page" alt="Previous page"/></a></li>
   <?php endif; ?>
  
   <?php foreach ($pager->getLinks() as $page): ?>
 	<?php if ($page == $pager->getPage()): ?>
       <li class="active"><?php echo $page ?></li>
 	<?php else: ?>
-	  <li><a href="<?php echo url_for('tweet') ?>?page=<?php echo $page ?>"><?php echo $page ?></a></li>
+	  <li><a href="<?php echo url_for('tweet') ?>?page=<?php echo $page ?>&n=<?php echo $twitterUser ?>"><?php echo $page ?></a></li>
 	<?php endif; ?>
 <?php endforeach; ?>
 
   <?php if($pager->getPage() == $pager->getLastPage()): ?>
   	<li class="next-off">Next &gt;</li>
   <?php else: ?>
-  <li class="next"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getNextPage() ?>"><img src="/images/next.png" title="Next page" alt="Next page"/></a></li>
-  <li class="next"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getLastPage() ?>">Last page &raquo;</a></li>
+  <li class="next"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getNextPage() ?>&n=<?php echo $twitterUser ?>"><img src="/images/next.png" title="Next page" alt="Next page"/></a></li>
+  <li class="next"><a href="<?php echo url_for('tweet') ?>?page=<?php echo $pager->getLastPage() ?>&n=<?php echo $twitterUser ?>">Last page &raquo;</a></li>
   <?php endif; ?>
 </ul>
 <?php endif; ?>
