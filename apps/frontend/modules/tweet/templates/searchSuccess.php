eea00df933a2e49b0b2320ea9a97400a4f7c25c1
Herbert Mühlburger <herbert.muehlburger@gmail.com>
2010-04-15 13:57:50 +0200

    deploying worked

--- apps/frontend/modules/tweet/templates/searchSuccess.php
+++ apps/frontend/modules/tweet/templates/searchSuccess.php
@@ -12,7 +12,9 @@
 <h3>Try out our Grabeeter Client</h3>
 <p>Grabeeter is a JavaFX application and enables you to search your tweets offline. You don't have to have an internet
 connection to search in your tweets. </p> 
-<a href="http://vlpc01.tugraz.at/projekte/herbert/tweetex/web/Grabeeter.jnlp" title="Grabeeter"><img src="images/jws-launch-button.jpg" title="Grabeeter Client" alt="Grabeeter Client" /></a>
+<a href="http://vlpc01.tugraz.at/projekte/herbert/t/dist/Grabeeter.jnlp" title="Grabeeter"><img src="images/jws-launch-button.gif" title="Grabeeter Client" alt="Grabeeter Client" /></a>
+<br />
+<a href="http://vlpc01.tugraz.at/projekte/herbert/t/dist/Grabeeter.html">Access Grabeeter Site</a>
 
 <h3>Results</h3>
 <?php include_partial('tweet/list', array('tweets' => $tweets))?>
