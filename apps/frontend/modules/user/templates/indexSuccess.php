713d517658f51affcaf8af6474437afd8e8cebba
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-05-20 17:07:46 +0200

    user interface updated

--- apps/frontend/modules/user/templates/indexSuccess.php
+++ apps/frontend/modules/user/templates/indexSuccess.php
@@ -1,8 +1,5 @@
 <div>
 	 <h3>List of Users</h3>
-
-    <?php foreach ($tweet_users as $tweet_user): ?>
-	  <a href="<?php echo url_for('user_show', $tweet_user) ?>"><img src="<?php echo $tweet_user->getProfileImageUrl() ?>" /></a>
-    <?php endforeach; ?>
+<?php include_partial('user/list', array('tweet_users' => $tweet_users)) ?>
 
 </div>
--- apps/frontend/templates/layout-960.php
+++ apps/frontend/templates/layout-960.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php include_slot('title', 'Grabeeter - Grab and Search your Tweets') ?></title>
     <?php use_javascript('jquery-1.4.2.min.js') ?>
     <?php use_javascript('search.js') ?>
@@ -30,12 +30,14 @@
         <div id="col2" class="grid_3 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
     </div>
      	<div id="content" class="grid_6">
-        <?php echo $sf_content ?>
+	    Search:<form action="#" method="get" enctype="text/plain"><input type="text" name="username" value="<?php if (isset($_POST['username'])) { echo $username; } ?>" size="20" maxlength="20"></form>
+       <?php echo $sf_content ?>
     </div>
     <div id="sidebar3" class="grid_5">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <div id="col1" class="grid_3 alpha">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-        <div id="col2" class="grid_2 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <div id="col1" class="grid_3 alpha">Add your twitter username:<form action="#" method="get" enctype="text/plain"><input type="text" name="username" value="<?php if (isset($_POST['username'])) { echo $username; } ?>" size="20" maxlength="20"></form></div>
+    <div id="col2" class="grid_2 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>        
+        
     </div>
 	<div id="footer" class="grid_16">
     	<p>&copy; 2010 Social Learning - Graz University of Technology | Terms of Usage | Privacy Policy</p>
