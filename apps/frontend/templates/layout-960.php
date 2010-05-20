dc06129b5524045e96d374ad1d402e78da21f93c
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-05-20 16:12:49 +0200

    landing page updated

--- apps/frontend/templates/layout-960.php
+++ apps/frontend/templates/layout-960.php
@@ -10,11 +10,11 @@
     <link rel="shortcut icon" href="/favicon.ico" />
   </head>
   <body>
-  <div id="container" class="container_12">
-  <div id="header" class="grid_12">
+  <div id="container" class="container_16">
+  <div id="header" class="grid_16">
 		<h1 id="logo"><a href="<?php echo url_for('homepage') ?>"><?php include_slot('title', 'Grabeeter - Search your Tweets') ?></a></h1>
   </div>
-  <div id="navbar" class="grid_12">
+  <div id="navbar" class="grid_16">
         <ul> 
 			<li><a href="<?php echo url_for('homepage') ?>">Home</a></li>
 			<li><a href="<?php echo url_for('@tweet') ?>">Tweets</a></li>
@@ -24,21 +24,20 @@
 			<li><a href="#">User Guide</a></li> 
 		</ul>
 	</div>
-	
-	<div id="content" class="grid_12">
+    <div id="sidebar1" class="grid_5">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div id="col1" class="grid_2 alpha">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+        <div id="col2" class="grid_3 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+    </div>
+     	<div id="content" class="grid_6">
         <?php echo $sf_content ?>
     </div>
-    <div id="sidebar2" class="grid_6">
+    <div id="sidebar3" class="grid_5">
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         <div id="col1" class="grid_3 alpha">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-        <div id="col2" class="grid_3 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+        <div id="col2" class="grid_2 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
     </div>
-     <div id="sidebar2" class="grid_6">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <div id="col1" class="grid_3 alpha">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-        <div id="col2" class="grid_3 omega">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
-    </div>
-	<div id="footer" class="grid_12">
+	<div id="footer" class="grid_16">
     	<p>&copy; 2010 Social Learning - Graz University of Technology | Terms of Usage | Privacy Policy</p>
     </div><!-- end footer -->    
   </div> 
