aba81ab43af6378fc110b7d85fafffcf9cedbc4e
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-05-20 17:25:16 +0200

    bug fixed

---
+++ apps/frontend/modules/user/templates/_list.php
@@ -0,0 +1,3 @@
+<?php foreach ($tweet_users as $tweet_user): ?>
+        <a href="<?php echo url_for('user_show', $tweet_user) ?>"><img src="<?php echo $tweet_user->getProfileImageUrl() ?>" /></a>
+<?php endforeach; ?>
