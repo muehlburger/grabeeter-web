4bca3d15104e7b2f847c29e60de362c9e3068608
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-05-27 08:57:21 +0200

    cron updated

--- runUpdate.sh
+++ runUpdate.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+if [ -e "lock" ]; then
+  echo "waiting ..."
+  exit;
+fi
 
+touch lock;
 cat usernames | while read line; do
   ./symfony updateTweets $line --env=prod
   if [ "$?" = "1" ]; then
+    rm lock;
     exit;
   fi
 done
--- runUpdate_dev.sh
+++ runUpdate_dev.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-echo
-date
+if [ -e "lock" ]; then
+  echo "waiting ..."
+  exit;
+fi
 
+touch lock;
 cat usernames | while read line; do
   ./symfony updateTweets $line --env=dev
   if [ "$?" = "1" ]; then
+    rm lock;
     exit;
   fi
 done
