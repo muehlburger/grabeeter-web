a867775ecd613627dab20e24adfee3bfcca8ea62
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-07 19:17:30 +0200

    link bug updated

--- apps/frontend/templates/layout-960.php
+++ apps/frontend/templates/layout-960.php
@@ -29,11 +29,8 @@
     </div><!-- end content -->
     <div id="sidebar" class="grid_5 round">
     	  <?php if(!include_slot('sidebar')): ?>
-    	  <h2>More Information</h2>		  
-		  <p class="subheader">
-			  <strong>Description | Offers</strong>, by <em>Herbert Mühlburger</em>
-		  </p>
-		    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    	  <h2>Grabeeter</h2>
+			  <p>Grabeeter - Grab and Search your Tweets</p>
 		  <?php endif; ?>        
     </div>
 	<div id="footer" class="grid_16 round">
