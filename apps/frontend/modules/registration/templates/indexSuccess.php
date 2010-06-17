acb2d800ca71ecf439fe927d0ed6ab8fa20391ba
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-17 10:46:56 +0200

    registration and search updated

--- apps/frontend/modules/registration/templates/indexSuccess.php
+++ apps/frontend/modules/registration/templates/indexSuccess.php
@@ -1,4 +1,6 @@
-<h2>Registration</h2>
+<h2>Tell us your Username</h2>
+<p>Here you can register your Twitter username. Registering your username enables you to 
+use Grabeeter. We cannot grab your tweets without knowing your username.</p>
 <form action="<?php echo url_for('@registration') ?>" method="POST">
 	<?php echo $form ?>
 	<input type="submit" value="Submit" />
--- apps/frontend/modules/tweet/templates/_search.php
+++ apps/frontend/modules/tweet/templates/_search.php
@@ -1,9 +1,11 @@
 <div class="search">
+
 <form action="<?php echo url_for('@tweet_search') ?>" method="get">
-  <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="query" />
+  <label for="query">Enter your search query (web, elearning, ...):</label>
+  <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="query" class="iround" />
   <input type="hidden" name="screen_name" value="<?php echo $sf_request->getParameter('screen_name') ?>" id="screen_name" />
   <img id="loader" src="/images/loader.gif" style="vertical-align: middle; display: none"/>
   <input type="submit" value="search" />
-  <div class="help">Enter some keywords (eg. web, eLearning, ...)</div>
+
 </form>
 </div>
