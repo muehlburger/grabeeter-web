3f6b916564a8de602f0284cee846a6d2e4b9eb1d
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-17 17:25:55 +0200

    default user modified

--- apps/frontend/modules/user/templates/indexSuccess.php
+++ apps/frontend/modules/user/templates/indexSuccess.php
@@ -1,5 +1,6 @@
 <div>
- <h2>Users of Grabeeter</h2>
-	 <p>The following you can seen all the Twitter users which are currently on Grabeeter.</p>
+ <h2>Search Tweets</h2>
+	 <p>The following you can seen all the Twitter users which are currently on Grabeeter. In order to 
+	 search you have to click on a specific user.</p>
 	<div><?php include_partial('user/list', array('tweet_users' => $tweet_users)) ?></div>
 </div>
