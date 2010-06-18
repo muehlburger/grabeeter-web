d9df239bf0bedaf389b6b1a4f88448574aef5abe
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-18 16:39:42 +0200

    version tagged

---
+++ apps/frontend/modules/help/templates/_sidebar.php
@@ -0,0 +1,16 @@
+<h2>Grabeeter lets you</h2>
+<ul>
+	<li>Search in your Tweets</li>
+	<li>Filter your Tweets by date</li>
+	<li>Search in your Tweets offline using the Grabeeter Client</li>
+	<li>Filter your tweets offline using the Grabeeter Client</li>
+	<li>Grabeeter provides an <?php echo link_to('API', '@help_api')?></li>
+</ul>
+<br />
+<h2>Launch Grabeeter</h2>
+<p>In order to launch Grabeeter Client just press the "Launch" button. This will
+download a JNLP File and start Grabeeter Client.</p>
+<div id="launch">
+<a href="http://grabeeter.tugraz.at/Grabeeter.jnlp" title="Grabeeter"><img
+	src="/images/jws-launch-button.gif" title="Grabeeter" alt="Grabeeter" /></a>
+</div>
---
+++ apps/frontend/modules/help/templates/helpSuccess.php
@@ -0,0 +1,58 @@
+<h2>Grabeeter User Guide</h2>
+
+<h3>Dear Grabeeter-User</h3>
+<p>Grabeeter is an application that allows you to search tweets of Twitter users registered on Grabeeter and to save the results locally.
+To become registered on Grabeeter just type in your Twitter name into the textfield below "Register" and press return.
+Your tweets will be added to the searchable pool of tweets on Grabeeter for anybody using Grabeeter.
+Only tweets of Twitter users registered on Grabeeter can be searched.
+</p>
+
+<h3>Read more:</h3>
+<ul>
+	<li>The Grabeeter Web-interface</li>
+	<li>The Grabeeter Desktop Application</li>
+	<li>Grabeeter for Developers</li>
+</ul>
+
+<h3>The Grabeeter Web-interface</h3>
+<h4>The Web-interface offers to:</h4>
+<ul>
+	<li>Register Twitter users to Grabeeter and become their tweets searchable by doing so</li>
+	<li>Search tweets of all Twitter users registered on Grabeeter online</li>
+	<li>Install the Grabeeter application for desktop usage</li>
+</ul>
+
+<p>To search tweets just type in the search keyword into the intended textfield "Tweetsearch" in the middle of the interface.
+The search will start instantly you typed in at least three characters. Pressing return will start the search as well.
+The results are displayed in a list below, listed by time latest above.
+</p>
+
+<h3>The Grabeeter Desktop Application</h3>
+<h4>The application offers you to:</h4>
+<ul>
+	<li>Search tweets of users registered on Grabeeter</li>
+	<li>Grab your latest tweets in order to be included in the search</li>
+	<li>Once you "grabbed" your tweets you do not have to be online to search you tweets</li>
+</ul>
+
+<h4>Installing and Removing Grabeeter</h4>
+<p>
+To use the Grabeeter Desktop Application click on the "Launch" button. The application will install and start automatically; it will leave a shortcut on your desktop after you finished.
+To remove Grabeeter from your local system again, open your software control panel, look for Grabeeter and click on button "Remove" or "Deinstall"; confirm. 
+</p>
+
+<h4>Grab your Tweets with Grabeeter</h4>
+<p>
+Within the application you can search tweets of Twitter users registered on Grabeete by typing in the username of a Twitter user registered on Grabeeter. 
+Confirm by hitting "Return" or the button "Grab Tweets".
+By doing so, tweets of that Twitter user will be downloaded to your system and stored in a <?php echo link_to('location described in the FAQs', '@help_faq#tweets-location')?> section.
+</p>
+
+<h4>Search your Tweets with Grabeeter</h4>
+<p>
+Type in your search keyword into the intended textfield "Search" and press return or click on button "Search". The results are shown within the textarea below the form.
+It is also possible to enter a time period to filter your tweets. Just enter the start of the time peroid into the intended textfield "From" and the end of the peroid into the intended textfield "To". 
+The results are displayed in the list below, listed by relevance.
+To see details of a result just click on the specific result; the details are displayed below the list of results.
+For Grabeeter offline use, it is required to retrieve the tweets of a Twitter user registered on Grabeeter first; Then you can start your search. 
+</p>
