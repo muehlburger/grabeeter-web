c62762457f2fe9911678a3b6a52f2110fa441ba5
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-18 16:36:53 +0200

    some little fixes

--- apps/frontend/modules/registration/templates/indexSuccess.php
+++ apps/frontend/modules/registration/templates/indexSuccess.php
@@ -1,6 +1,8 @@
 <h2>Tell us your Username</h2>
 <p>Here you can register your Twitter username. Registering your username enables you to 
 use Grabeeter. We cannot grab your tweets without knowing your username.</p>
+<p>After the registration it my last some time until you show up on Grabeeter. This is normal and 
+we are working on improving this behaviour.</p>
 <form action="<?php echo url_for('@registration') ?>" method="POST">
 	<?php echo $form ?>
 	<input type="submit" value="Submit" />
--- apps/frontend/modules/user/actions/actions.class.php
+++ apps/frontend/modules/user/actions/actions.class.php
@@ -15,7 +15,7 @@ class userActions extends sfActions
 	{
 		$this->tweet_users = Doctrine::getTable('TweetUser')
 		->createQuery('a')
-		->limit(11)
+//		->limit(11)
 		->execute();
 	}
 
