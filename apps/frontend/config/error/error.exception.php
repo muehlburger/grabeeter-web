f6ed30c6953c9be5c3e8fb9659c2c97f18ddb6fa
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-19 09:01:12 +0200

    partials added

---
+++ apps/frontend/config/error/error.exception.php
@@ -0,0 +1,3 @@
+Oops! It seems that the page was not found.
+
+Go back to <?php echo link_to('Grabeeter Home', '@homepage')?>. 
---
+++ apps/frontend/modules/help/templates/_launchgrabeeter.php
@@ -0,0 +1,8 @@
+<br />
+<h2>Launch Grabeeter</h2>
+<p>In order to launch Grabeeter Client just press the "Launch" button. This will
+download a JNLP File and start Grabeeter Client.</p>
+<div id="launch">
+<a href="http://grabeeter.tugraz.at/Grabeeter.jnlp" title="Grabeeter"><img
+	src="/images/jws-launch-button.gif" title="Grabeeter" alt="Grabeeter" /></a>
+</div>
