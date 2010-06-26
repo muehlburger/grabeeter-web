5074c872e30d458918348980727dcf83272af6b4
Herbert Muehlburger <herbert.muehlburger@gmail.com>
2010-06-26 07:55:54 +0200

    piwik added

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -47,5 +47,17 @@
 </div>
 <!-- end footer --></div>
 <!-- end container -->
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://analytics.muehlburger.at/" : "http://analytics.muehlburger.at/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://analytics.muehlburger.at/piwik.php?idsite=2" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tag -->
 </body>
 </html>
